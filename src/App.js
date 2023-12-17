2c143f98d0c05ea224914b8f66fcb412716d341f
deepakkr14 <deepakbipin142@gmail.com>
2023-12-17 14:19:24 +0530

    initiial expense

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import ExpenseItem from './component/ExpenseItem';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   <ExpenseItem></ExpenseItem>
+   <pre> <ul>
+    <li>Expenses    Items</li>
+    <li> Food       Rs 10</li>
+    <li> Petrol     Rs 100 </li>
+    <li> Movies      Rs 200</li>
+   </ul></pre>
+   </div>
   );
 }
 
---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,4 @@
+ function ExpenseItem(){
+    return <h1>Expense Items</h1>
+ } 
+ export default ExpenseItem;
