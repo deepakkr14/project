bb234d6267e790c45771fd47ace8bd43603870b3
deepakkr14 <deepakbipin142@gmail.com>
2023-12-22 19:39:40 +0530

    implementing conditional rendering

--- src/App.js
+++ src/App.js
@@ -38,22 +38,41 @@ const App = () => {
     return data.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent=<h2>no data available</h2>;
+  if (filteredData.length ===1){
+    expensesContent= 
+    // <h4>Only single Expense here. Please add more...</h4>
+  (filteredData.map((props) => (
+    <div className="expense-item" key={props.id}>
+      <ExpenseDetails
+        amount={props.amount}
+        location={props.location}
+        title={props.title}
+        date={props.date}
+      />
+      <h4>Only single Expense here. Please add more...</h4>
+    </div>
+  )))}
+  else {
+    expensesContent=(filteredData.map((props) => (
+      <div className="expense-item" key={props.id}>
+        <ExpenseDetails
+          amount={props.amount}
+          location={props.location}
+          title={props.title}
+          date={props.date}
+        />
+      </div>
+    )))
+    }
   return (
     <div className="expenses-list">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredData.map((props) => (
-        <div className="expense-item" key={props.id}>
-          <ExpenseDetails
-            amount={props.amount}
-            location={props.location}
-            title={props.title}
-            date={props.date}
-          />
-        </div>
-      ))}
+      {/* {filteredData.length===0?(<p>no data available</p>):()} */}
+     {expensesContent}
       <Form onaddExpense={addExpenseHandler} />
     </div>
   );
